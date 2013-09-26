64cfec3517b023009175e5e78368dd22a71dfa84
Alessandro Secco <seccoale@gmail.com>
2013-09-26 16:55:13 +0200

    first commit: main method to run pagerank given a BVGraph

---
+++ src/it/unipd/dei/run_utils/LinkAnalysisRunner.java
@@ -0,0 +1,56 @@
+package it.unipd.dei.run_utils;
+
+import java.io.IOException;
+import java.security.InvalidAlgorithmParameterException;
+import java.util.Scanner;
+
+import it.unimi.dsi.big.webgraph.BVGraph;
+import it.unipd.dei.algorythms.MyGraphUtils;
+
+public class LinkAnalysisRunner {
+	private final static String PAGE_RANK="page rank";
+	private final static String END="end";
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		if(args.length<1){
+			System.err.println("usage:\njava -jar <jar_name.jar> <basename_bvGraph>");
+			System.exit(1);
+		}
+		try {
+			BVGraph graph=BVGraph.load(args[0]);
+			Scanner in=new Scanner(System.in);
+			String command=null;
+			do{
+				System.out.println(PAGE_RANK+" - executes page rank");
+				System.out.println(END+" - close the application");
+				command=in.nextLine();
+				switch (command) {
+				case PAGE_RANK:
+					System.out.println("write alpha:");
+					double alpha=in.nextDouble();
+					System.out.println("write epsilon");
+					double epsilon=in.nextDouble();
+					System.out.println("write max iteration");
+					int maxIterations=in.nextInt();
+					try {
+						double[] page_rank_res=MyGraphUtils.GetPageRank(graph, alpha, epsilon, maxIterations);
+						for(double d: page_rank_res){
+							System.out.print(d+" - ");
+						}
+					} catch (InvalidAlgorithmParameterException e) {
+						e.printStackTrace();
+					}
+					break;
+
+				default:
+					break;
+				}
+			} while (END.equals(command));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
